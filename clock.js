bddb5175f2c9bf13210da584fcb71cade1285bc4
EdoRyunosuke <76624835+EdoRyunosuke@users.noreply.github.com>
2021-03-12 18:09:06 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import SwitchBW from './switchBW' 
+
+export default class Clock extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            date: 0, 
+            year: 0, 
+            month: 0, 
+            daynum: 0, 
+            hour: 0,
+            minute: 0,
+            second: 0,
+            day: '',
+            style: '',
+            interval: false}
+        let curDate = new Date()
+        
+        this.getClocks ()
+        this.timerId = 0;
+    }
+
+    getClocks () {
+        let curDate = new Date()
+
+        this.setState({
+            date: curDate.getDate(),
+            year: curDate.getFullYear(),
+            month: curDate.getMonth(),
+            daynum: curDate.getDay(),
+            hour: curDate.getHours(),
+            minute: curDate.getMinutes(),
+            second: curDate.getSeconds(),
+        })
+    }
+
+    switchday () {
+        switch(this.state.daynum) {
+            case 1:
+                this.setState({day: '月曜'})
+                break
+            
+            case 2:
+                this.setState({day: '火曜'})
+                break
+
+            case 3:
+                this.setState({day: '水曜'})
+                break
+            
+            case 4:
+                this.setState({day: '木曜'})
+                break
+            
+            case 5:
+                this.setState({day: '金曜'})
+                break
+            
+            case 6:
+                this.setState({day: '土曜'})
+                break
+
+            case 7:
+                this.setState({day: '日曜'})
+                break
+        }
+    }
+
+    switchBWcontrol () {
+        if (SwitchBW.state.btnh === false){
+            this.setState({style: '{background-color: white;} h1{color: black;}'})
+        }
+
+        if (SwitchBW.state.btnh === true) {
+            this.setState({style: '{background-color: black;} h1{color: white;}'})
+        }
+    }
+
+    componentWillMount () {
+        this.timerId = setInterval (e => {this.tick()}, 1000)
+      }
+      
+    componentWillUnmount () {
+        clearInterval(this.timerId)
+      }
+      
+    tick () {
+        this.getClocks()
+        this.switchday()
+      }
+
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.hour}:{this.state.minute}:{this.state.second}</h1><br/>
+                <h2>{this.state.day}, {this.state.year}/{this.state.month}/{this.state.date}</h2>
+            </div>
+        )
+    }
+}
